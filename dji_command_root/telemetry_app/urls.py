38b7ebf09d1c676bc2f194e77a8e9155f013f539
杨耀端 <2096240090@qq.com>
2025-11-05 19:39:34 +0800

    多级表

--- dji_command_root/telemetry_app/urls.py
+++ dji_command_root/telemetry_app/urls.py
@@ -4,7 +4,8 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 # 导入所有 ViewSet
 from .views import AlarmViewSet, AlarmCategoryViewSet, WaylineViewSet
-
+from django.conf import settings
+from django.conf.urls.static import static
 router = DefaultRouter()
 router.register(r'alarms', AlarmViewSet)
 router.register(r'alarm-categories', AlarmCategoryViewSet, basename='alarmcategory')
@@ -13,3 +14,5 @@ router.register(r'waylines', WaylineViewSet)  # 注册航线视图集
 urlpatterns = [
     path('', include(router.urls)),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
